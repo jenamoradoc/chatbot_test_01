fac2e716a93920cad69c9695b94ac2947f6ff1b4
Enamorado <jenamorado>
2018-03-06 17:23:13 -0300

    Prueba uno chatbot

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAASESL1dAFgBACY0q4X6ZA8Oplk4fWOqXKLO3HOHb56BP9NL1tdEAaSxPTDT6yPFdsZCHM8w0jhQWxqxlFNfT8WLxLZBY9Oy3hNYUxg6twlJqAIhuDQbNwO1dfcBtvjcKN2YPa9ZCWbkEOoUW1rCVELrji8ApGaWTMoZCfaHhKwZDZD',
+    FB_VERIFY_TOKEN: 'token_de_verificacion_P_001',
+    API_AI_CLIENT_ACCESS_TOKEN: '5a6ede898b0746f2ace219a2ef6bd9e4',
+    FB_APP_SECRET: '',
+    SERVER_URL: "https://gchatbootest001.herokuapp.com/",
+};
